6eb99aa4de6fd1ef7bafc6197cb4bdb2c7ee9f91
Darth Varg <cbvargas123@gmail.com>
2020-04-21 09:48:31 -0700

    Commented out morgan; commented out insertion console log

--- database-mongodb/rawMongoSeed.js
+++ database-mongodb/rawMongoSeed.js
@@ -2,19 +2,23 @@ const generateData = require('./dataGenerator').generateData;
 const getBenchmark = require('./dataGenerator').getBenchmark;
 const fs = require('fs')
 const { db } = require('./index.js');
+const colors = require('colors');
 
 const totalStart = process.hrtime.bigint();
 
 async function seed(set, batch, time) {
-    let insertCount = 0
+    // let insertCount = 0
+    console.log('Seeding Data...'.yellow)
     let data = generateData(set, batch)
     for (let products of data) {
-        insertCount += products.length;
+        // insertCount += products.length;
         await db.collection('products').insertMany(products)
-        console.log(`inserted so far: ${insertCount}`)
+            // console.log(`inserted so far: ${insertCount}`)
     }
+    console.log('Data Seeded!'.green)
+
     totalEnd = process.hrtime.bigint();
     console.log(getBenchmark(time, totalEnd, 'Total', set * batch))
 }
-seed(1000, 10000, totalStart)
+seed(10000, 1000, totalStart)
     .then(() => db.close())
